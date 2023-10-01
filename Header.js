9e48e885c4904d0768cb177ebc1547655fc380e8
SUMAREDDY2003 <130961098+SUMAREDDY2003@users.noreply.github.com>
2023-10-01 13:08:44 +0530

    Update Header.js

--- Header.js
+++ Header.js
@@ -24,6 +24,16 @@ const Header = () => {
             <Tab LinkComponent={NavLink} to="/add" label='Add Product'/>
             <Tab LinkComponent={NavLink} to="/books" label='Books'/>
             <Tab LinkComponent={NavLink} to="/about" label='About Us'/>
+          </Tabs>
+          <Tabs 
+           sx = {{ml : 'auto'}}
+           textColor="inherit" 
+           indicatorColor="secondary" 
+           value={value} 
+           onChange={(e,val)=>setValue(val)}>  
+            <Tab  LinkComponent={NavLink} to="/login" label='Login'/>
+            <Tab LinkComponent={NavLink} to="/signup" label='Signup'/>
+            
         </Tabs>
         </Toolbar>
          
