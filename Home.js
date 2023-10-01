99aaf1c886c21b973e5d5949e50f962729a3d4a5
SUMAREDDY2003 <130961098+SUMAREDDY2003@users.noreply.github.com>
2023-10-01 13:09:16 +0530

    Update Home.js

--- Home.js
+++ Home.js
@@ -1,4 +1,5 @@
 import { Box, Button, Typography } from '@mui/material';
+import {  yellow } from '@mui/material/colors';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
@@ -12,8 +13,23 @@ const Home = () => {
       </Typography>
       </Button>
      </Box>
+     <Box display="flex" flexDirection="row" justifyContent="center" bgcolor={yellow} p={6} > 
+      
+      <Button  LinkComponent={Link} to="/login" sx={{marginTop:10, marginRight:2, backgroundColor:'grey'}} variant='contained'>
+      <Typography variant='h3'>
+        Login
+      </Typography>
+      </Button>
+     
+      <Button  LinkComponent={Link} to="/signup" sx={{marginTop:10, backgroundColor:'grey'}} variant='contained'>
+      <Typography variant='h3'>
+        Signup
+      </Typography>
+      </Button>
+     </Box>
+     
     </div>
   )
 }
 
-export default Home
+export default Home;
